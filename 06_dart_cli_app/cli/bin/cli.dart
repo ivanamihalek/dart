3cf76aebe3bb7f3827d0615410734dfef335e5e3
Ivana Mihalek <ivana.mihalek@gmail.com>
2026-03-13 00:45:59 +0100

    argparser

--- 06_dart_cli_app/cli/bin/cli.dart
+++ 06_dart_cli_app/cli/bin/cli.dart
@@ -6,54 +6,6 @@ import 'package:command_runner/command_runner.dart';
 const version = '0.0.1';
 
 void main(List<String> arguments) async {
-    var runner = CommandRunner(); // Create an instance of your new CommandRunner
+    var runner = CommandRunner()..addCommand(HelpCommand()); // Create an instance of your new CommandRunner
     await runner.run(arguments); // Call its run method, awaiting its Future<void>
-}
-
-void searchWikipedia(List<String>? arguments) async { // Add this new function and add ? to arguments type
-  final String? articleTitle;
-
-  // If the user didn't pass in arguments, request an article title.
-  if (arguments == null || arguments.isEmpty) {
-    print('Please provide an article title.');
-    // Await input and provide a default empty string if the input is null.
-    final inputFromStdin = stdin.readLineSync(); // Read input
-    if (inputFromStdin == null || inputFromStdin.isEmpty) {
-      print('No article title provided. Exiting.');
-      return; // Exit the function if no valid input
-    }
-    articleTitle = inputFromStdin;
-
-  } else {
-    // Otherwise, join the arguments into a single string.
-    articleTitle = arguments.join(' ');
-  }
-
-  print('Looking up articles about "$articleTitle". Please wait...');
-  // Call the API and await the result
-  var articleContent = await getWikipediaArticle(articleTitle);
-  print(articleContent); // Print the full article response (raw JSON for now)
-
-}
-
-Future<String> getWikipediaArticle(String articleTitle) async {
-  final url = Uri.https(
-    'en.wikipedia.org', // Wikipedia API domain
-    '/api/rest_v1/page/summary/$articleTitle', // API path for article summary
-  );
-  final response = await http.get(url); // Make the HTTP request
-
-  if (response.statusCode == 200) {
-    return response.body; // Return the response body if successful
-  }
-
-  // Return an error message if the request failed
-  return 'Error: Failed to fetch article "$articleTitle". Status code: ${response.statusCode}';
-
-}
-
-void printUsage() { // Add this new function
-  print(
-      "The following commands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
-  );
 }
--- 06_dart_cli_app/command_runner/lib/src/arguments.dart
+++ 06_dart_cli_app/command_runner/lib/src/arguments.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+import 'dart:collection';
 import '../command_runner.dart';
 
 enum OptionType { flag, option }
@@ -53,13 +55,11 @@ class Option extends Argument {
   }
 }
 
-// Add this class below the Option class
+
 abstract class Command extends Argument {
   @override
   String get name;
-
   String get description;
-
   bool get requiresArgument => false;
 
   late CommandRunner runner;
@@ -73,5 +73,80 @@ abstract class Command extends Argument {
   @override
   String? valueHelp;
 
+  final List<Option> _options = [];
+  UnmodifiableSetView<Option> get options =>
+      UnmodifiableSetView(_options.toSet());
+
+    // A flag is an [Option] that's treated as a boolean.
+  void addFlag(String name, {String? help, String? abbr, String? valueHelp}) {
+    _options.add(
+      Option(
+        name,
+        help: help,
+        abbr: abbr,
+        defaultValue: false,
+        valueHelp: valueHelp,
+        type: OptionType.flag,
+      ),
+    );
+  }
+
+  // An option is an [Option] that takes a value.
+  void addOption(
+    String name, {
+    String? help,
+    String? abbr,
+    String? defaultValue,
+    String? valueHelp,
+  }) {
+    _options.add(
+      Option(
+        name,
+        help: help,
+        abbr: abbr,
+        defaultValue: defaultValue,
+        valueHelp: valueHelp,
+        type: OptionType.option,
+      ),
+    );
+  }
+
+  FutureOr<Object?> run(ArgResults args);
+  @override
+  String get usage {
+    return '$name:  $description';
+  }
+
 }
 
+
+class ArgResults {
+  Command? command;
+  String? commandArg;
+  Map<Option, Object?> options = {};
+
+  // Returns true if the flag exists.
+  bool flag(String name) {
+    // Only check flags, because we're sure that flags are booleans.
+    for (var option in options.keys.where(
+      (option) => option.type == OptionType.flag,
+    )) {
+      if (option.name == name) {
+        return options[option] as bool;
+      }
+    }
+    return false;
+  }
+
+  bool hasOption(String name) {
+    return options.keys.any((option) => option.name == name);
+  }
+
+  ({Option option, Object? input}) getOption(String name) {
+    var mapEntry = options.entries.firstWhere(
+      (entry) => entry.key.name == name || entry.key.abbr == name,
+    );
+
+    return (option: mapEntry.key, input: mapEntry.value);
+  }
+}
